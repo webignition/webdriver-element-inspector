fb76e804a8321e193fc4df582f5f6c83eb30dcd7
Jon Cram <webignition@gmail.com>
2019-08-30 11:58:52 +0100

    Create Inspector::getValue() for textarea elements (#10)

--- src/Inspector.php
+++ src/Inspector.php
@@ -6,6 +6,9 @@ use Facebook\WebDriver\WebDriverElement;
 
 class Inspector
 {
+    const INPUT_ELEMENT_TAG_NAME = 'input';
+    const TEXTAREA_TAG_NAME = 'textarea';
+
     const INPUT_ELEMENT_VALUE_ATTRIBUTE = 'value';
 
     public static function create(): Inspector
@@ -15,6 +18,16 @@ class Inspector
 
     public function getValue(WebDriverElement $element): ?string
     {
-        return $element->getAttribute(self::INPUT_ELEMENT_VALUE_ATTRIBUTE);
+        $tagName = $element->getTagName();
+
+        if (self::INPUT_ELEMENT_TAG_NAME === $tagName) {
+            return $element->getAttribute(self::INPUT_ELEMENT_VALUE_ATTRIBUTE);
+        }
+
+        if (self::TEXTAREA_TAG_NAME === $tagName) {
+            return $element->getText();
+        }
+
+        return '';
     }
 }
