b87550dab5d8e23f6aa918aae3f3a0efacc23aec
Jon Cram <jon@webignition.net>
2019-08-30 11:26:10 +0100

    Create Navigator::getValue() for input element, add unit tests

--- src/Inspector.php
+++ src/Inspector.php
@@ -2,6 +2,19 @@
 
 namespace webignition\WebDriverElementInspector;
 
+use Facebook\WebDriver\WebDriverElement;
+
 class Inspector
 {
+    const INPUT_ELEMENT_VALUE_ATTRIBUTE = 'value';
+
+    public static function create(): Inspector
+    {
+        return new Inspector();
+    }
+
+    public function getValue(WebDriverElement $element): ?string
+    {
+        return $element->getAttribute(self::INPUT_ELEMENT_VALUE_ATTRIBUTE);
+    }
 }
--- tests/Unit/InspectorTest.php
+++ tests/Unit/InspectorTest.php
@@ -1,15 +1,63 @@
 <?php
+/** @noinspection PhpDocSignatureInspection */
 
 namespace webignition\WebDriverElementInspector\Tests\Unit;
 
+use Facebook\WebDriver\WebDriverElement;
 use webignition\WebDriverElementInspector\Inspector;
 
 class InspectorTest extends \PHPUnit\Framework\TestCase
 {
-    public function testCreate()
-    {
-        $inspector = new Inspector();
+    /**
+     * @var Inspector
+     */
+    private $inspector;
 
-        $this->assertInstanceOf(Inspector::class, $inspector);
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->inspector = Inspector::create();
+    }
+
+    /**
+     * @dataProvider getValueDataProvider
+     */
+    public function testGetValue(WebDriverElement $element, ?string $expectedValue)
+    {
+        $this->assertSame($expectedValue, $this->inspector->getValue($element));
+    }
+
+    public function getValueDataProvider()
+    {
+        $hasEmptyValueAttributeElement = \Mockery::mock(WebDriverElement::class);
+        $hasEmptyValueAttributeElement
+            ->shouldReceive('getAttribute')
+            ->andReturn('');
+
+        $hasNotEmptyValueAttributeElement = \Mockery::mock(WebDriverElement::class);
+        $hasNotEmptyValueAttributeElement
+            ->shouldReceive('getAttribute')
+            ->andReturn('value content');
+
+        $doesNotHaveValueAttributeElement = \Mockery::mock(WebDriverElement::class);
+        $doesNotHaveValueAttributeElement
+            ->shouldReceive('getAttribute')
+            ->andReturn(null);
+
+        return [
+            'has empty value attribute' => [
+                'element' => $hasEmptyValueAttributeElement,
+                'expectedValue' => '',
+            ],
+            'has non-empty value attribute' => [
+                'element' => $hasNotEmptyValueAttributeElement,
+                'expectedValue' => 'value content',
+            ],
+            'does not have value attribute' => [
+                'element' => $doesNotHaveValueAttributeElement,
+                'expectedValue' => null,
+            ],
+        ];
     }
 }
