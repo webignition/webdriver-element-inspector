3fc33d8d674be2399a0c949e459a62e28c68a58d
Jon Cram <webignition@gmail.com>
2019-09-24 17:38:51 +0100

    Make all but Inspector::getValue() private (#23)

--- tests/Functional/InspectorTest.php
+++ tests/Functional/InspectorTest.php
@@ -1,14 +1,13 @@
 <?php
+/** @noinspection PhpUnhandledExceptionInspection */
 /** @noinspection PhpDocSignatureInspection */
 
 declare(strict_types=1);
 
 namespace webignition\WebDriverElementInspector\Tests\Functional;
 
-use Facebook\WebDriver\WebDriverElement;
-use webignition\WebDriverElementCollection\RadioButtonCollection;
-use webignition\WebDriverElementCollection\SelectOptionCollection;
-use webignition\WebDriverElementCollection\WebDriverElementCollection;
+use webignition\SymfonyDomCrawlerNavigator\Model\ElementLocator;
+use webignition\SymfonyDomCrawlerNavigator\Navigator;
 use webignition\WebDriverElementInspector\Inspector;
 
 class InspectorTest extends AbstractTestCase
@@ -26,21 +25,45 @@ class InspectorTest extends AbstractTestCase
     }
 
     /**
-     * @dataProvider getElementValueDataProvider
+     * @dataProvider getValueDataProvider
      */
-    public function testGetElementValue(string $fixture, string $elementCssSelector, ?string $expectedValue)
+    public function testGetValue(string $fixture, string $elementCssSelector, ?string $expectedValue)
     {
         $crawler = self::$client->request('GET', $fixture);
-        $element = $crawler->filter($elementCssSelector)->getElement(0);
+        $navigator = Navigator::create($crawler);
+        $collection = $navigator->find(new ElementLocator($elementCssSelector));
 
-        if ($element instanceof WebDriverElement) {
-            $this->assertSame($expectedValue, $this->inspector->getElementValue($element));
-        }
+        $this->assertSame($expectedValue, $this->inspector->getValue($collection));
     }
 
-    public function getElementValueDataProvider(): array
+    public function getValueDataProvider(): array
     {
         return [
+            'radio button collection, none checked' => [
+                'fixture' => '/form.html',
+                'elementCssSelector' => 'input[type="radio"][name="radio-not-checked"]',
+                'expectedValue' => null,
+            ],
+            'radio button collection, has checked' => [
+                'fixture' => '/form.html',
+                'elementCssSelector' => 'input[type="radio"][name="radio-checked"]',
+                'expectedValue' => 'checked-2',
+            ],
+            'select option collection, none selected' => [
+                'fixture' => '/form.html',
+                'elementCssSelector' => 'select[name="select-none-selected"] option',
+                'expectedValue' => 'none-selected-1',
+            ],
+            'select option collection, has selected' => [
+                'fixture' => '/form.html',
+                'elementCssSelector' => 'select[name="select-has-selected"] option',
+                'expectedValue' => 'has-selected-3',
+            ],
+            'collection of unrelated elements' => [
+                'fixture' => '/form.html',
+                'elementCssSelector' => 'input',
+                'expectedValue' => null,
+            ],
             'input, empty value' => [
                 'fixture' => '/form.html',
                 'elementCssSelector' => 'input[name="input-with-empty-value"]',
@@ -66,16 +89,6 @@ class InspectorTest extends AbstractTestCase
                 'elementCssSelector' => 'textarea[name="non-empty-textarea"]',
                 'expectedValue' => 'textarea content',
             ],
-            'input[type=radio] not checked' => [
-                'fixture' => '/form.html',
-                'elementCssSelector' => 'input[name="radio-not-checked"]',
-                'expectedValue' => 'not-checked-1',
-            ],
-            'input[type=radio] checked' => [
-                'fixture' => '/form.html',
-                'elementCssSelector' => 'input[name="radio-checked"]',
-                'expectedValue' => 'checked-1',
-            ],
             'paragraph, content with no line returns' => [
                 'fixture' => '/content.html',
                 'elementCssSelector' => '.p1',
@@ -92,105 +105,5 @@ class InspectorTest extends AbstractTestCase
                 'expectedValue' => 'Single non-breaking line wrapped in many line returns',
             ],
         ];
-    }
-
-    /**
-     * @dataProvider getRadioGroupValueDataProvider
-     */
-    public function testGetRadioGroupValue(string $fixture, string $elementCssSelector, ?string $expectedValue)
-    {
-        $crawler = self::$client->request('GET', $fixture);
-        $elements = $crawler->filter($elementCssSelector);
-        $collectionElements = [];
-
-        foreach ($elements as $element) {
-            $collectionElements[] = $element;
-        }
-
-        $collection = new RadioButtonCollection($collectionElements);
-
-        $this->assertSame($expectedValue, $this->inspector->getRadioGroupValue($collection));
-    }
-
-    public function getRadioGroupValueDataProvider(): array
-    {
-        return [
-            'input[type=radio] not checked' => [
-                'fixture' => '/form.html',
-                'elementCssSelector' => 'input[name="radio-not-checked"]',
-                'expectedValue' => null,
-            ],
-            'input[type=radio] checked' => [
-                'fixture' => '/form.html',
-                'elementCssSelector' => 'input[name="radio-checked"]',
-                'expectedValue' => 'checked-2',
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider getSelectOptionGroupValueDataProvider
-     */
-    public function testGetSelectOptionGroupValue(
-        string $fixture,
-        string $elementCssSelector,
-        ?string $expectedValue
-    ) {
-        $crawler = self::$client->request('GET', $fixture);
-        $elements = $crawler->filter($elementCssSelector);
-        $collectionElements = [];
-
-        foreach ($elements as $element) {
-            $collectionElements[] = $element;
-        }
-
-        $collection = new SelectOptionCollection($collectionElements);
-
-        $this->assertSame($expectedValue, $this->inspector->getSelectOptionGroupValue($collection));
-    }
-
-    public function getSelectOptionGroupValueDataProvider(): array
-    {
-        return [
-            'select, none selected' => [
-                'fixture' => '/form.html',
-                'elementCssSelector' => 'select[name="select-none-selected"] option',
-                'expectedValue' => 'none-selected-1',
-            ],
-            'select, has selected' => [
-                'fixture' => '/form.html',
-                'elementCssSelector' => 'select[name="select-has-selected"] option',
-                'expectedValue' => 'has-selected-3',
-            ],
-        ];
-    }
-
-    public function testGetValue()
-    {
-        $crawler = self::$client->request('GET', '/form.html');
-
-        $singleInputCollection = new WebDriverElementCollection([
-            $crawler->filter('input[name="input-with-non-empty-value"]')->getElement(0),
-        ]);
-
-        $radioElements = [];
-        $radioCrawler = $crawler->filter('input[type="radio"][name="radio-checked"]');
-        foreach ($radioCrawler as $radioButton) {
-            $radioElements[] = $radioButton;
-        }
-
-        $radioButtonCollection = new RadioButtonCollection($radioElements);
-
-        $optionElements = [];
-        $optionCrawler = $crawler->filter('select[name="select-has-selected"] option');
-        foreach ($optionCrawler as $option) {
-            $optionElements[] = $option;
-        }
-
-        $selectOptionGroupCollection = new SelectOptionCollection($optionElements);
-
-        $this->assertSame('value content', $this->inspector->getValue($singleInputCollection));
-        $this->assertSame('checked-2', $this->inspector->getValue($radioButtonCollection));
-        $this->assertSame('has-selected-3', $this->inspector->getValue($selectOptionGroupCollection));
     }
 }
